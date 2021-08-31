87d6fc62e631fb4f0d133024425a9394659f4f09
usmansafk <usman230@hotmail.co.uk>
2021-08-31 17:07:38 +0100

    v2 logic

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
-//logic
-//select b1 b2 b3 and h2 via getElemById()
-//click event for each
-//when dec- h2--. if h2 < 0 color = red
-
-const number = document.getElementById("num");
+const number = document.getElementById("value");
 const decrease = document.getElementById("minus");
 const reset = document.getElementById("reset");
 const increase = document.getElementById("add");
 
-let num = 0;
-number.textContent = num;
-number.style.fontSize = "5rem";
+let count = 0;
+number.textContent = count;
 
 decrease.addEventListener("click", function () {
-  number.textContent = `${num--}`;
-  if (num < 0) {
+  if (count < 0) {
     number.style.color = "red";
   }
+  number.textContent = `${count--}`;
 });
 
 reset.addEventListener("click", function () {
-  num = 0;
+  count = 0;
   number.textContent = `0`;
+  if (count == 0) {
+    number.style.color = "black";
+  }
 });
 
 increase.addEventListener("click", function () {
-  number.textContent = `${num++}`;
-  if (num > 0) {
+  if (count > 0) {
     number.style.color = "green";
   }
+  number.textContent = `${count++}`;
 });
